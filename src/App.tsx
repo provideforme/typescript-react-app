e3480f1c64e4daebcf9da92cb30b40aa17e367a9
provideforme <evan.a.bernard@gmail.com>
2022-05-20 11:02:06 -0400

    delete function

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,17 @@ const App: React.FC = () => {
 
   const todoAddHandler = (text: string) => {
     setTodos(prevTodos => ([...prevTodos,{id: Math.random().toString(), text: text}]));
-    
+  }
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
   }
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 };
